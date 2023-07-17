8e5f51f9b05414749a333f5a77c4915af3ba3729
Moses Etyang <108185671+Geek0254@users.noreply.github.com>
2023-07-17 23:22:25 +0300

    Update 1 : Error Handling
    Initially, our code assumed that the user will always select a valid image file and that the OCR process will be successful. 
    To handle this, we validate the selected file path and display appropriate error messages if the file is not found or if the OCR process fails.

---
+++ image_to_text_ocr.py
@@ -0,0 +1,56 @@
+import cv2
+import pytesseract
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
+import clipboard
+from tkinter import ttk
+
+def select_image():
+    # Open a file dialog to select an image
+    root = tk.Tk()
+    root.withdraw()
+    file_path = filedialog.askopenfilename(filetypes=[("Image files", ".png;.jpeg;*.jpg")])
+    
+    # Validate the file path
+    if file_path:
+        try:
+            # Load the image
+            image = cv2.imread(file_path)
+
+            # Perform OCR on the image
+            text = pytesseract.image_to_string(image)
+
+            # Display the recognized text
+            if text:
+                messagebox.showinfo("Recognized Text", text)
+            else:
+                messagebox.showwarning("No Text Detected", "No text was found in the selected image.")
+        except Exception as e:
+            messagebox.showerror("Error", str(e))
+    else:
+        messagebox.showwarning("No Image Selected", "Please select an image file.")
+
+def copy_text():
+    text = select_image()
+    if text:
+        clipboard.copy(text)
+        messagebox.showinfo("Success", "Text Copied to Clipboard")
+
+# Create a GUI with a button to select an image and recognize text
+root = tk.Tk()
+root.title("Image to Text Recognition")
+
+root.configure(bg='#F9F9F9')
+
+select_button = tk.Button(root, text="Select Image", command=select_image, bg='#FFC107', activebackground='#FFA000', 
+                         activeforeground='black', bd=0, highlightthickness=0, font=('Arial', 12), relief='flat')
+select_button.config(width=20, height=2)
+select_button.pack(pady=10)
+
+copy_button = tk.Button(root, text="Copy Text", command=copy_text, bg='#FFC107', activebackground='#FFA000', 
+                       activeforeground='black', bd=0, highlightthickness=0, font=('Arial', 12), relief='flat')
+copy_button.config(width=20, height=2)
+copy_button.pack(pady=10)
+
+root.mainloop()
